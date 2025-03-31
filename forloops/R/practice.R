4186da40e8d716153a87ce987dfbeba7ccef4512
carsonic <cc4254a@american.edu>
2025-03-31 19:34:41 -0400

    Add documentation to code for part c

--- forloops/R/practice.R
+++ forloops/R/practice.R
@@ -1,4 +1,18 @@
 # part c code
+#' @title Column Means Function
+#' @details
+#' It allows you to compute the means of colums in a given data frame
+#'  
+#' @param data_frame a data frame
+#' @return a vector of numeric column means
+#' @export
+#' 
+#' @examples
+#' ex_data1 <- data.frame(a = c(1, 2, 3, 4), b = c(5, 6, 7, 8))
+#' col_means(ex_data1)
+#' 
+#' ex_data2 <- data.frame(a = c(2, 4, 6, NA), b = c(8, 10, 12))
+#' col_means(ex_data2)
 col_means <- function(data_frame) {
   means <- numeric(ncol(data_frame))  
   for (i in seq_along(data_frame)) {
@@ -8,6 +22,15 @@ col_means <- function(data_frame) {
 }
 
 # part d code
+#' @title NA Count Function
+#' @details
+#' It allows you to count the number of NA values that appears in a given vector
+#'  
+#' @param vector a vector
+#' @return an integer count of NA values
+#' @export
+#' 
+#' @examples
 count_na <- function(vector) {
   count <- 0
   for (value in vector) {
