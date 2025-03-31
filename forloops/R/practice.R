68650d384d2f3e60c5c719f8590ca679e8dccdb5
carsonic <cc4254a@american.edu>
2025-03-31 18:05:49 -0400

    Add code for forloops package (parts c and d)

--- forloops/R/practice.R
+++ forloops/R/practice.R
@@ -1,1 +1,19 @@
 # part c code
+col_means <- function(data_frame) {
+  means <- numeric(ncol(data_frame))  
+  for (i in seq_along(data_frame)) {
+    means[i] <- mean(data_frame[[i]], na.rm = TRUE)
+  }
+  return(means)
+}
+
+# part d code
+count_na <- function(vector) {
+  count <- 0
+  for (value in vector) {
+    if (is.na(value)) {
+      count <- count + 1
+    }
+  }
+  return(count)
+}
